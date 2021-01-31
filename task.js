05afe4ab237422d0aef659e14542c073678d124b
Nenakhov.Max@yandex.ru <Nenakhov.Max@yandex.ru>
2021-01-31 17:22:30 +0500

    исправление ошибок

--- task.js
+++ task.js
@@ -1,20 +1,26 @@
-document.getElementById('modal_main').className = 'modal modal_active';
-let modalClose = document.getElementsByClassName('modal__close modal__close_times');
-let firstModalClose = modalClose.item(0);
-firstModalClose.onclick = modalCloses;
-modalClose.item(1).onclick = successModalCloses;
-let doGreat = document.getElementsByClassName('btn btn_danger modal__close show-success');
-doGreat.item(0).onclick = showGreat;
+const modalMain = document.getElementById('modal_main');
+const modalSuccess = document.getElementById('modal_success');
+const btnSuccess = document.getElementsByClassName('btn_danger')[0];
+const btnClose = Array.from(document.getElementsByClassName('modal__close_times'));
 
-function showGreat() {
-    modalCloses();
-    document.getElementById('modal_success').className = 'modal modal_active';
+function showModalMain() {
+    modalMain.classList.add('modal_active');
 }
 
-function modalCloses() {
-    document.getElementById('modal_main').className = 'modal';
+function clickBtn() {
+    modalMain.classList.remove('modal_active');
+    modalSuccess.classList.add('modal_active');
 }
 
-function successModalCloses() {
-    document.getElementById('modal_success').className = 'modal';
+function closeForm() {
+    console.log(this.closest('div.modal'));
+    this.closest('div.modal').classList.remove('modal_active');
+}
+
+
+document.addEventListener('DOMContentLoaded', showModalMain);
+btnSuccess.addEventListener('click', clickBtn);
+for (let i = 0; i < btnClose.length; i++) {
+    const element = btnClose[i];
+    element.addEventListener('click', closeForm);
 }
